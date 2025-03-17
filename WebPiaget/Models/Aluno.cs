31c9e40d7ecb2ebb12545d7180d8fd5d673915b4
luizbenicio2004 <luizbenicio007@gmail.com>
2025-03-17 19:27:12 -0300

    Adicionar arquivos de projeto.

---
+++ WebPiaget/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace WebPiaget.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Escola { get; set; }
+
+        public String Pai { get; set; }
+
+        public String Mae { get; set; }
+
+        public String RGM { get; set; }
+
+        public DateTime Idade { get; set; }
+
+        public decimal Notas { get; set; }
+
+        public string? Professor { get; set; }
+    }
+}
